28782b8bd40e7c820b78f0852de86da0af713d5b
Jorge Vega <j.vega@mefasa.org>
2021-10-25 14:31:59 -0700

    Cap. 12 - Definicion de variables

---
+++ TiposDeDatos.java
@@ -0,0 +1,12 @@
+class TiposDeDatos{
+
+    public static void main(String[] args){
+        int edad = 18;
+        System.out.println("Mi edad es: " + edad);
+        edad = 19;
+        System.out.println("Mi edad es: " + edad);
+        edad = 20;
+        System.out.println("Mi edad es: " + edad);
+    }
+
+}
