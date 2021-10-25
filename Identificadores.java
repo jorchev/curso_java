db5884bd0cf451840012da37666455965d759fd0
Jorge Vega <j.vega@mefasa.org>
2021-10-25 14:47:30 -0700

    Cap. 14 - Uso de identificadores

---
+++ Identificadores.java
@@ -0,0 +1,9 @@
+public class Identificadores{
+
+    public static void main(String[] args){
+        double $_sue1do = 100000.45;
+        double inicio = 100000.10;
+        double final = 100000.65; // identificador definido como palabra reservada, esto no compila. 
+    }
+
+}
