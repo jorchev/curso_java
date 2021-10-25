a0ab6876c431a495d3089703a1dfc59585018c8f
Jorge Vega <j.vega@mefasa.org>
2021-10-25 14:36:52 -0700

    Cap. 13 - Definicion de constantes

--- TiposDeDatos.java
+++ TiposDeDatos.java
@@ -1,12 +1,9 @@
 class TiposDeDatos{
 
     public static void main(String[] args){
-        int edad = 18;
+        final int edad = 18;
         System.out.println("Mi edad es: " + edad);
-        edad = 19;
-        System.out.println("Mi edad es: " + edad);
-        edad = 20;
-        System.out.println("Mi edad es: " + edad);
+
     }
 
 }
