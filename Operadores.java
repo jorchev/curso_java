177b623ffcd7167b19412663090c92d2e59ffac4
Jorge Vega <jorge.hvr@gmail.com>
2021-10-25 21:11:13 -0700

    Cap. 16 - Operadores aritmeticos

---
+++ Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    
+    public static void main(String[] args){
+
+        // Operadores aritmeticos
+        int suma = 10 + 20;
+        System.out.println(suma);
+
+        int resta = 20 - 10;
+        System.out.println(resta);
+
+        int multiplicacion = 20 * 10;
+        System.out.println(multiplicacion);
+
+        int division = 20 / 10;
+        System.out.println(division);
+
+        int modulo = 20 % 10;
+        System.out.println(modulo);
+
+    }
+
+}
