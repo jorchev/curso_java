f192dae77a61ab3e700961a668737a6c5e24c23f
Jorge Vega <jorge.hvr@gmail.com>
2021-10-25 21:38:35 -0700

    Cap. 18 - Operadores de comparacion

---
+++ OperadoresDeComparacion.java
@@ -0,0 +1,17 @@
+public class OperadoresDeComparacion {
+    
+    public static void main(String[] args){
+
+        double sueldo1 = 10000.5322;
+        double sueldo2 = 43223.2311;
+
+        System.out.println(sueldo1>sueldo2);
+        System.out.println(sueldo1<sueldo2);
+        System.out.println(sueldo1>=sueldo2);
+        System.out.println(sueldo1<=sueldo2);
+        System.out.println(sueldo1!=sueldo2);
+        System.out.println(sueldo1==sueldo2);
+
+    }
+
+}
